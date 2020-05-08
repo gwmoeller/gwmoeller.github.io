0b70a11fb81af654c64d679b7522e9c0f97c2c37
gwmoeller <53918840+gwmoeller@users.noreply.github.com>
2020-05-08 15:38:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$.getJSON("https:api.guildwars2.com/v2/guild/32525C62-CE73-EA11-81AC-95DFE50946EB/treasury?access_token=6C82128E-774C-714A-B5E5-0ED0ECD1660DC1A2435C-2735-45D8-9A54-B710D22AB313", function(data){
+$.getJSON("https://api.guildwars2.com/v2/guild/32525C62-CE73-EA11-81AC-95DFE50946EB/treasury?access_token=6C82128E-774C-714A-B5E5-0ED0ECD1660DC1A2435C-2735-45D8-9A54-B710D22AB313", function(data){
 	var obj = [];
 	var count =[];
 	var need = [];
